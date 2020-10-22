30c14b9b936f8c0f67e46c5f820f9403e8122bc2
Daniel Nael <daniel.nael.002@gmail.com>
2020-10-22 11:38:12 +0300

    Initation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
 public class Main
 {
+    private Game currentGame;
+
+    public Game getCurrentGame() { return currentGame; }
+
+    public void setCurrentGame(Game currentGame) { this.currentGame = currentGame; }
+
 }
--- src/test/java/SensorTest.java
+++ src/test/java/SensorTest.java
@@ -1,5 +1,5 @@
 import static org.junit.Assert.*;
 
-public class Tests
-{
+public class SensorTest {
+
 }
